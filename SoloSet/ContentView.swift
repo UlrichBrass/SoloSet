9919ac7ffa2a84bebafebf83408d6e425d4146f6
Uli <ulrich.brass@outlook.de>
2020-06-05 12:12:19 +0200

    Initial Commit

---
+++ SoloSet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoloSet
+//
+//  Created by Ulrich Braß on 05.06.20.
+//  Copyright © 2020 Ulrich Braß. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
